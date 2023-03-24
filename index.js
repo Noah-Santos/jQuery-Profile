84af4e3a4acc506b5aba586f32801032da8f25c7
NoahSantos <nsanto591@west-mec.org>
2023-03-23 18:14:46 -0700

    Nav
    Started to create the nav section for each of the different sections of the profile.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 $(function(){
-    // $("#searchInput").slideDown(0);
+    // variable to hold count variable
+    let searchCount = 1;
+    // one method
     $("#search").click(function(){
-        $("#searchInput").slideDown(500).animate({width:"7rem"}, 500).animate({height:"1.5rem"}, 500);
-        // $("#searchInput").slideUp(500);
+        // if count variable is 1, open search and increase variable by 1
+        if(searchCount == 1){
+            searchCount++;
+            // second and third method
+            $("#searchInput").animate({height:"1.5rem"}, 500).slideDown(500).animate({width:"8rem"}, 500);
+        // if count variable is 2, close search and decrease variable by 2
+        }else if(searchCount == 2){
+            searchCount--;
+            // fourth method
+            $("#searchInput").animate({height:"0rem"}, 500).animate({width:"0rem"}, 500).slideUp(500);
+        }
     });
 })
