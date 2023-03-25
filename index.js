df929c97c321ffc3a7cc82fdeda27bd971c0c56d
Noah-Santos <nsanto591@west-mec.org>
2023-03-24 22:14:05 -0700

    Content
    Added content to the page and got it to transition, just need to add more information.

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 $(function(){
     // variable to hold count variable
     let searchCount = 1;
-    // one method
     // when click on search icon
     $("#search").click(function(){
         // if count variable is 1, open search and increase variable by 1
         if(searchCount == 1){
             searchCount++;
-            // second and third method
             $("#searchInput").animate({height:"1.5rem"}, 500).slideDown(200).animate({width:"8rem"}, 500);
         // if count variable is 2, close search and decrease variable by 2
         }else if(searchCount == 2){
             searchCount--;
-            // fourth method
             $("#searchInput").animate({height:"0rem"}, 500).animate({width:"0rem"}, 500).slideUp(200);
         }
     });
 
-    // fifth method
     // when you enter a nav section, it will change background color
     $(".navNavigation").mouseenter(function(){
-        // sixth method
         // var to hold id of current hovered element
         var temp = $(this).attr("id");
-        // seventh method
         // changes the background color to slightly darker
         $("#" + temp).css("background-color","rgb(50, 50, 62)");
-        // eigth method
         $("#" + temp).mouseout(function(){
             let arr = $("#" + temp).attr("class");
             let classes = arr.split(" ");
@@ -39,21 +32,27 @@ $(function(){
         });
     });
 
-    // ninth method
     // when you click a nav section, it will change background color and change content displayed
     $("span.navNavigation").on("click",function(){
-        // tenth and eleventh method  
         // gets rid of active class on all span tags and resets color to normal except on the one clicked
         $("span").removeClass("active");
         $("span").not(this).css("background-color","rgb(62, 62, 82)");
-        // twelfth method
         // adds active class to currently clicked section
         $(this).addClass("active");
+        // holds id of clicked element
+        let id = $(this).attr("id");
+
+        // slipes up the content section, then changes the content changes, and finally displays it again
+        $(".content").slideUp(700, function(){
+            // hides all of the active sections
+            $(".section").removeClass("activeSect");
+            // adds active class to the sections that was selected to be displayed
+            $("#" + id + "Cont").addClass("activeSect");
+        }).slideDown(700);
     });
 
     // when click over logout button
     $("#logOut").click(function(){
-        // thirteenth, fourteenth, and fifteenth method
         // fades it out on click and waits 2 seconds before fade in
         $(this).fadeOut(300).delay(2000).fadeIn(300);
     });
